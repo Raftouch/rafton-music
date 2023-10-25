9b3381f87d7aa9176789d004084706cc2007fbcb
Raftouch <rafanelkin@gmail.com>
2023-10-25 18:39:10 +0200

    :card_file_box: db connected

--- server/index.js
+++ server/index.js
@@ -3,8 +3,20 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 
+const mysql = require('mysql')
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'music-app',
+})
+
+db.connect(() => {
+  console.log('MySql connected')
+})
+
 app.get('/', (req, res) => {
-    res.json('Hello from Api !')
+  res.json('Hello from Api !')
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`))
