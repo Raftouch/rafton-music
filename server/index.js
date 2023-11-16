5159191b760ef922f84166f97537600daf536c77
Raftouch <rafanelkin@gmail.com>
2023-11-16 11:26:00 +0100

    :bug: fixed bugs db & docker

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ app.use(cors())
 const mysql = require('mysql')
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 })
 
