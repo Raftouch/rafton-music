5b341114f0c233e40f3e7e571f719fa146e341ad
Raftouch <rafanelkin@gmail.com>
2024-02-07 19:01:34 +0100

    feat :sparkles: add navbar with routes

---
+++ client/src/app/playlists/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Button from '@/components/Button'
+
+export default function Playlists() {
+  return (
+    <div>
+      <h1>Discover Playlists</h1>
+      <Button onClick={() => console.log('open modal')}>Upload New</Button>
+    </div>
+  )
+}
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function Navbar() {
+  const pathname = usePathname()
+
+  return (
+    <div className="w-screen h-[80px] border-b-2 flex justify-between items-center p-5">
+      <span>LOGO</span>
+
+      <ul className="flex space-x-10">
+        <Link href="/" className={pathname === '/' ? 'text-teal-400' : ''}>
+          Home
+        </Link>
+        <Link
+          href="/playlists"
+          className={pathname === '/playlists' ? 'text-teal-400' : ''}
+        >
+          Playlists
+        </Link>
+        <Link
+          href="/favourites"
+          className={pathname === '/favourites' ? 'text-teal-400' : ''}
+        >
+          Favourites
+        </Link>
+      </ul>
+    </div>
+  )
+}
