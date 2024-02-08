ceff693fdd3aec5af9a811c22dbdb78bf15d1ca7
Raftouch <rafanelkin@gmail.com>
2024-02-08 13:17:12 +0100

    feat :card_file_box: add data models

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,3 +1,3 @@
 export default function NotFound() {
-  return <div>Sorry, Buddy, the page could not be found 😞</div>
+  return <div>Sorry, Buddy, the page could not be found 😞</div>;
 }
---
+++ client/src/models/song.ts
@@ -0,0 +1,21 @@
+export interface Song {
+  id: string
+  title: string
+  image?: string
+  audio: string
+  playcount: number
+  uploadedat: Date
+  artists: Artist[]
+  genres: Genre[]
+  //   users?: User[]
+}
+
+export interface Artist {
+  id: string
+  name: string
+}
+
+export interface Genre {
+  id: string
+  type: string
+}
