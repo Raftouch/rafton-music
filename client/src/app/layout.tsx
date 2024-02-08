ffbb6aeae818bae7ea8e0668c206b802172f4ad7
Raftouch <rafanelkin@gmail.com>
2024-02-08 12:46:47 +0100

    feat :sparkles: add customized not-found page

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Rafton',
-  description: 'Created by Nelli with NextJS & much love',
-}
+  title: "Rafton",
+  description: "Created by Nelli with NextJS & much love",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -26,5 +26,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
