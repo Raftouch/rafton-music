e00d8c98c936046f0653f9816c2df99f78969261
Raftouch <rafanelkin@gmail.com>
2024-02-10 16:18:38 +0100

    feat :sparkles: add create song page & wrapper component

--- client/src/app/(SSR)/songs/create/page.tsx
+++ client/src/app/(SSR)/songs/create/page.tsx
@@ -1,5 +1,33 @@
-import React from 'react'
+'use client'
+
+import Button from '@/components/Button'
+import SongInfo from '@/components/SongInfo'
+import StepWrapper from '@/components/StepWrapper'
+import React, { useState } from 'react'
 
 export default function CreateSong() {
-  return <div>Create Song</div>
+  const [activeStep, setActiveStep] = useState(0)
+
+  const next = () => {
+    if (activeStep !== 2) {
+      setActiveStep((prev) => prev + 1)
+    }
+  }
+  const back = () => {
+    setActiveStep((prev) => prev - 1)
+  }
+
+  return (
+    <StepWrapper activeStep={activeStep}>
+      {activeStep == 0 && <SongInfo />}
+
+      {/* {activeStep == 1 && <h1 className="text-slate-900">{activeSteps[1]}</h1>} */}
+      {/* {activeStep == 2 && <h1 className="text-slate-900">{activeSteps[2]}</h1>} */}
+
+      <div className="text-slate-900 flex justify-between mt-10">
+        <Button onClick={back}>Back</Button>
+        <Button onClick={next}>Next</Button>
+      </div>
+    </StepWrapper>
+  )
 }
---
+++ client/src/components/StepWrapper.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+interface StepWrapperProps {
+  activeStep: number
+  children: React.ReactNode
+}
+
+export default function StepWrapper({
+  activeStep,
+  children,
+}: StepWrapperProps) {
+  const steps = ['Song information', 'Ipload image', 'Upload audio']
+
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center">
+        <div className="w-full">
+          <ul className="flex justify-between">
+            {steps.map((step, index) => (
+              <li key={index}>
+                <div
+                  className={`w-8 h-8 text-center rounded-full border-2 ${
+                    activeStep > index ? 'border-blue-500' : 'border-gray-300'
+                  }`}
+                >
+                  <span
+                    className={`${
+                      activeStep > index ? 'text-blue-500' : 'text-gray-500'
+                    }`}
+                  >
+                    {index + 1}
+                  </span>
+                </div>
+                <span
+                  className={`${
+                    activeStep > index ? 'text-blue-500' : 'text-gray-500'
+                  }`}
+                >
+                  {step}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-14">
+        <div className="w-full max-w-3xl">
+          <div className="bg-white p-8 rounded-lg shadow-md">{children}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
