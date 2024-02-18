e830a32e36c851dd1fcee688a0397a8e216ae679
Raftouch <rafanelkin@gmail.com>
2024-02-18 18:34:17 +0100

    feat :dizzy: create song on client ok

--- client/src/app/(SSR)/songs/create/page.tsx
+++ client/src/app/(SSR)/songs/create/page.tsx
@@ -1,30 +1,66 @@
-"use client";
+'use client'
 
-import Button from "@/components/Button";
-import FileUpload from "@/components/FileUpload";
-import SongInfo from "@/components/SongInfo";
-import StepWrapper from "@/components/StepWrapper";
-import React, { useState } from "react";
+import Button from '@/components/Button'
+import FileUpload from '@/components/FileUpload'
+import StepWrapper from '@/components/StepWrapper'
+import { useInput } from '@/hooks/useInput'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function CreateSong() {
-  const [activeStep, setActiveStep] = useState(0);
-  const [image, setImage] = useState(null);
-  const [audio, setAudio] = useState(null);
+  const [activeStep, setActiveStep] = useState(0)
+  const [image, setImage] = useState('')
+  const [audio, setAudio] = useState('')
+  const title = useInput('')
+  const artist = useInput('')
+  const genre = useInput('')
+  const router = useRouter()
 
   const next = () => {
     if (activeStep !== 2) {
-      setActiveStep((prev) => prev + 1);
+      setActiveStep((prev) => prev + 1)
+    } else {
+      const formData = new FormData()
+      formData.append('title', title.value)
+      formData.append('artist_id', artist.value)
+      formData.append('genre_id', genre.value)
+      formData.append('image', image)
+      formData.append('audio', audio)
+      fetch('http://localhost:5000/api/songs', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => router.push('/songs'))
+        .catch((e) => console.log(e))
     }
-  };
+  }
+
   const back = () => {
     if (activeStep !== 0) {
-      setActiveStep((prev) => prev - 1);
+      setActiveStep((prev) => prev - 1)
     }
-  };
+  }
 
   return (
     <StepWrapper activeStep={activeStep}>
-      {activeStep === 0 && <SongInfo />}
+      {activeStep === 0 && (
+        <div className="p-5 w-[100%]">
+          <div className="flex flex-col gap-5">
+            <div className="flex gap-5">
+              <p className="w-1/6">Title</p>
+              <input className="w-full border-2" {...title} />
+            </div>
+            <div className="flex gap-5">
+              <p className="w-1/6">Artist</p>
+              <input className="w-full border-2" {...artist} />
+            </div>
+            <div className="flex gap-5">
+              <p className="w-1/6">Genre</p>
+              <input className="w-full border-2" {...genre} />
+            </div>
+          </div>
+        </div>
+      )}
 
       {activeStep === 1 && (
         <FileUpload setFile={setImage} accept="image/">
@@ -43,5 +79,5 @@ export default function CreateSong() {
         <Button onClick={next}>Next</Button>
       </div>
     </StepWrapper>
-  );
+  )
 }
