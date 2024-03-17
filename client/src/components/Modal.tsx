e5cf1843e700d5202454d75389f62ca5c777de6b
Raftouch <rafanelkin@gmail.com>
2024-03-17 16:12:47 +0100

    feat :sparkles: create custom modal

---
+++ client/src/components/Modal.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+interface ModalProps {
+  onClose: () => void
+  onDelete: () => void
+}
+
+export default function Modal({ onClose, onDelete }: ModalProps) {
+  return (
+    <>
+      <div
+        className="fixed bg-black/50 top-0 right-0 left-0 bottom-0"
+        onClick={onClose}
+      />
+      <div className="fixed min-w-[320px] min-h-[200px] p-10 rounded bg-white text-rafton-blue top-28 left-1/2 -translate-x-1/2">
+        <h1 className="text-center mb-10">Are you sure ?</h1>
+        <div className="flex justify-center gap-20">
+          <button
+            className="border rounded px-4 py-2 bg-green-500"
+            onClick={() => {
+              onDelete()
+              onClose()
+            }}
+          >
+            Yes
+          </button>
+          <button
+            className="border rounded px-4 py-2 bg-red-500"
+            onClick={onClose}
+          >
+            No
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
--- client/src/components/RemoveBtn.tsx
+++ client/src/components/RemoveBtn.tsx
@@ -3,27 +3,35 @@
 import { useRouter } from 'next/navigation'
 import Button from './Button'
 import { toast } from 'sonner'
+import { useState } from 'react'
+import Modal from './Modal'
 
 interface DeleteSongProps {
   id: string
 }
 
 export default function RemoveBtn({ id }: DeleteSongProps) {
+  const [modal, setModal] = useState(false)
   const router = useRouter()
+  const openModal = () => {
+    setModal(true)
+  }
+
   const removeSong = async () => {
-    const confirmed = confirm('Are you sure?')
+    const response = await fetch(`http://localhost:5000/api/songs/${id}`, {
+      method: 'DELETE',
+    })
+    toast.success('Song successfully removed')
 
-    if (confirmed) {
-      const response = await fetch(`http://localhost:5000/api/songs/${id}`, {
-        method: 'DELETE',
-      })
-      toast.success('Song successfully removed')
-
-      if (response.ok) {
-        router.refresh()
-      }
+    if (response.ok) {
+      router.refresh()
     }
   }
 
-  return <Button onClick={removeSong}>Remove</Button>
+  return (
+    <>
+      <Button onClick={openModal}>Remove</Button>
+      {modal && <Modal onClose={() => setModal(false)} onDelete={removeSong} />}
+    </>
+  )
 }
