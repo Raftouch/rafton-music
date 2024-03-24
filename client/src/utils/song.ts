ab788389560005976674a1a648620b9c835b3718
Raftouch <rafanelkin@gmail.com>
2024-03-24 19:26:00 +0100

    fix :monocle_face: data fetch

--- client/src/utils/song.ts
+++ client/src/utils/song.ts
@@ -1,38 +1,38 @@
-import { Song } from "@/models/song";
-import { notFound } from "next/navigation";
+import { Song } from '@/models/song'
+import { notFound } from 'next/navigation'
 
 export async function getSong(id: string): Promise<Song | null> {
   try {
     const response = await fetch(`http://localhost:5000/api/songs/${id}`, {
-      cache: "no-cache",
-    });
-    if (response.status === 404) notFound();
+      cache: 'no-cache',
+    })
+    if (response.status === 404) notFound()
 
     if (!response.ok) {
-      throw new Error("Failed to fetch song data");
+      throw new Error('Failed to fetch song data')
     }
-    const song: Song = await response.json();
-    return song;
+    const song: Song = await response.json()
+    return song
   } catch (error) {
-    console.error("Error:", error);
-    return null;
+    console.error('Error:', error)
+    return null
   }
 }
 
 export async function getAllSongs(): Promise<Song[] | null> {
   try {
-    const response = await fetch("http://localhost:5000/api/songs", {
-      next: { revalidate: 0 },
-    });
-    if (response.status === 404) notFound();
+    const response = await fetch('http://localhost:5000/api/songs', {
+      cache: 'no-cache',
+    })
+    if (response.status === 404) notFound()
 
     if (!response.ok) {
-      throw new Error("Failed to fetch song data");
+      throw new Error('Failed to fetch song data')
     }
-    const songs: Song[] = await response.json();
-    return songs;
+    const songs: Song[] = await response.json()
+    return songs
   } catch (error) {
-    console.error("Error:", error);
-    return null;
+    console.error('Error:', error)
+    return null
   }
 }
