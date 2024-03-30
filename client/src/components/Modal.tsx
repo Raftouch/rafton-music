0b944d3c3ee5b3f6ccc8c2dde4a15a0cbc58fcf4
Raftouch <rafanelkin@gmail.com>
2024-03-30 19:39:24 +0100

    test :white_check_mark: delete song ok

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react'
 
 interface ModalProps {
-  onClose: () => void;
-  onDelete: () => void;
+  onClose: () => void
+  onDelete: () => void
 }
 
 export default function Modal({ onClose, onDelete }: ModalProps) {
@@ -13,13 +13,13 @@ export default function Modal({ onClose, onDelete }: ModalProps) {
         onClick={onClose}
       />
       <div className="fixed min-w-[320px] min-h-[200px] p-10 rounded bg-white text-rafton-blue top-28 left-1/2 -translate-x-1/2">
-        <h1 className="text-center mb-10">Are you sure ?</h1>
+        <h1 className="text-center mb-10">Are you sure?</h1>
         <div className="flex justify-center gap-20">
           <button
             className="border rounded px-4 py-2 bg-green-500"
             onClick={() => {
-              onDelete();
-              onClose();
+              onDelete()
+              onClose()
             }}
           >
             Yes
@@ -33,5 +33,5 @@ export default function Modal({ onClose, onDelete }: ModalProps) {
         </div>
       </div>
     </>
-  );
+  )
 }
