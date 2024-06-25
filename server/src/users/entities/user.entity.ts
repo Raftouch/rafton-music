b52947642b10d63c73be72e1704576cf40b61280
Raftouch <rafanelkin@gmail.com>
2024-06-25 17:49:51 +0200

    create user entity

---
+++ server/src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Role, User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  email: string;
+
+  // password should not be recorded in Swagger, as it's sensitive data
+  password: string;
+
+  @ApiProperty({ default: Role.BASIC })
+  role: Role;
+
+  @ApiProperty({ default: Date.now() })
+  registeredAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
