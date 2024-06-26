6fa77f72e032b3efba96a6b1cae38e8bb2973893
Raftouch <rafanelkin@gmail.com>
2024-06-26 11:04:04 +0200

    add jwt strategy

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -13,8 +15,9 @@ import { PrismaModule } from 'src/prisma/prisma.module';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '15m' },
     }),
+    UsersModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
