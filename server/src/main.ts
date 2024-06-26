452044017fad88250977f098333d81efd0ab5d7d
Raftouch <rafanelkin@gmail.com>
2024-06-26 13:03:52 +0200

    add admin authorization for swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,6 +15,7 @@ async function start() {
     .setTitle('Rafton')
     .setDescription('The Rafton API description')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
