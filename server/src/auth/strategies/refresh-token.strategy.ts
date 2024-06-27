04c2fb034a8198b43219c7897b146ba37f756514
Raftouch <rafanelkin@gmail.com>
2024-06-27 12:47:30 +0200

    fix refresh token strategy

--- server/src/auth/strategies/refresh-token.strategy.ts
+++ server/src/auth/strategies/refresh-token.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -13,12 +13,18 @@ export class RefreshTokenStrategy extends PassportStrategy(
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // how i get a token
       secretOrKey: process.env.REFRESH_JWT_SECRET,
-      passReqToCallBack: true, // to refresh the token
+      passReqToCallback: true, // to refresh the token
     });
   }
 
   async validate(req: Request, payload: { id: string }) {
-    const refreshToken = req.get('authorization').replace('Bearer', '').trim();
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization header not found');
+    }
+
+    const refreshToken = authHeader.replace('Bearer', '').trim();
 
     return {
       ...payload,
