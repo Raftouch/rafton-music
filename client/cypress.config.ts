207458bce231a3f3da1edc4f0983d94515fe63d8
Raftouch <rafanelkin@gmail.com>
2024-10-01 13:27:38 +0200

    try

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-    },
-    defaultCommandTimeout: 50000,
+    setupNodeEvents(on, config) {},
+    defaultCommandTimeout: 100000,
   },
 })
