7de02f535966053fd36137a66ae440625272fd51
Raftouch <rafanelkin@gmail.com>
2024-10-01 13:49:58 +0200

    try

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -2,11 +2,8 @@ import { faker } from '@faker-js/faker'
 import 'cypress-file-upload'
 
 describe('songs on client', () => {
-  beforeEach(() => {
-    cy.login('rafa', '1234')
-  })
-
   it('should create a song', () => {
+    cy.login('rafa', '1234')
     cy.get('a').contains('Upload new').click()
     cy.url().should('include', '/songs/create')
     cy.get('input[name="title"]').type(faker.music.songName())
@@ -23,6 +20,7 @@ describe('songs on client', () => {
   })
 
   it('should read a song', () => {
+    cy.login('rafa', '1234')
     cy.get('li > a').eq(0).click()
     cy.get('[data-cy="song-details"]').should('exist')
     cy.get('button').eq(1).click()
@@ -32,6 +30,7 @@ describe('songs on client', () => {
   })
 
   it('should update a song', () => {
+    cy.login('rafa', '1234')
     cy.get('button').eq(2).click()
     cy.url().should('include', '/songs/edit')
     cy.get('[data-cy="input-title"]').clear().type(faker.music.songName())
@@ -40,6 +39,7 @@ describe('songs on client', () => {
   })
 
   it('should delete a song', () => {
+    cy.login('rafa', '1234')
     cy.get('button').eq(3).click()
     cy.get('div > h1').contains('Are you sure?').should('be.visible')
     cy.get('button').contains('Yes').click()
--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -15,6 +15,5 @@ Cypress.Commands.add('login', (username: string, password: string) => {
   cy.get('input[name="username"]').type(username)
   cy.get('input[name="password"]').type(password)
   cy.get('button[type="submit"]').click()
-  cy.contains('Login successful').should('be.visible')
   cy.url().should('include', '/songs')
 })
