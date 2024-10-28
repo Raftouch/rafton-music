b6f1ce66b6343c3a2823a2d682ac0a8cced439e5
Raftouch <rafanelkin@gmail.com>
2024-10-28 17:32:55 +0100

    just another commit

--- client/src/utils/const.ts
+++ client/src/utils/const.ts
@@ -1,6 +1,2 @@
-export const API_URL = 'http://localhost:5000'
-// export const API_URL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'http://portainer-cda3b.dev-formation.com:5000'
-//     : 'http://localhost:5000'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
--- server/src/main.ts
+++ server/src/main.ts
@@ -7,18 +7,14 @@ import * as cookieParser from 'cookie-parser';
 async function start() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5000;
-  const allowedOrigins = [
-    'http://localhost:3024',
-    'http://localhost:3000',
-    'http://portainer-cda3b.dev-formation.com:3024',
-  ];
+  const allowedOrigins = ['http://localhost:3024', 'http://localhost:3000'];
 
   app.use(cookieParser());
   app.enableCors({
     origin: allowedOrigins,
     credentials: true,
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    // allowedHeaders: 'Content-Type, Accept, Authorization, Cookie',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization, Cookie',
   });
   // app.useGlobalPipes(new ValidationPipe());
 
