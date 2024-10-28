83f06f3d0ba6adcc6b49bd65f8a8739aa0fa259b
Raftouch <rafanelkin@gmail.com>
2024-10-28 17:28:09 +0100

    updt cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,16 +7,15 @@ import * as cookieParser from 'cookie-parser';
 async function start() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5000;
-  // const allowedOrigins = [
-  //   'http://localhost:3024',
-  //   'http://localhost:3000',
-  //   'http://portainer-cda3b.dev-formation.com:3024',
-  // ];
+  const allowedOrigins = [
+    'http://localhost:3024',
+    'http://localhost:3000',
+    'http://portainer-cda3b.dev-formation.com:3024',
+  ];
 
   app.use(cookieParser());
   app.enableCors({
-    origin: true,
-    // origin: allowedOrigins,
+    origin: allowedOrigins,
     credentials: true,
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // allowedHeaders: 'Content-Type, Accept, Authorization, Cookie',
