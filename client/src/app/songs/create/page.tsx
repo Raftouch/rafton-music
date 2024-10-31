89604e4a19132dc687c823a41ce1d98d0e67b5c7
Raftouch <rafanelkin@gmail.com>
2024-10-31 12:03:56 +0100

    update create song with auth

--- client/src/app/songs/create/page.tsx
+++ client/src/app/songs/create/page.tsx
@@ -1,5 +1,32 @@
-import CreateSongForm from '@/components/CreateSongForm'
+'use client'
 
-export default async function CreateSong() {
+import CreateSongForm from '@/components/CreateSongForm'
+import Loader from '@/components/Loader'
+import useUserStore from '@/store/user'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+export default function CreateSong() {
+  const [loadingAuth, setLoadingAuth] = useState(true)
+  const router = useRouter()
+
+  const { checkAuth, isAuth } = useUserStore()
+
+  useEffect(() => {
+    const authenticateUser = async () => {
+      await checkAuth()
+      setLoadingAuth(false)
+
+      const { isAuth } = useUserStore.getState()
+
+      if (!isAuth) {
+        router.push('/auth/login')
+      }
+    }
+    authenticateUser()
+  }, [checkAuth, isAuth, router])
+
+  if (loadingAuth) return <Loader />
+
   return <CreateSongForm />
 }
