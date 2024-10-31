4c9101e87c994ab25f105a47d4b9b4132bd0495a
Raftouch <rafanelkin@gmail.com>
2024-10-31 16:28:21 +0100

    updt cors middleware - test

--- server/cors.middleware.ts
+++ server/cors.middleware.ts
@@ -3,9 +3,21 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CORSMiddleware implements NestMiddleware {
+  // Define allowed origins
+  private allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3024',
+    'http://portainer-cda3b.dev-formation.com:3024',
+  ];
+
   use(req: Request, res: Response, next: NextFunction) {
     const origin = req.headers.origin;
-    res.header('Access-Control-Allow-Origin', origin);
+
+    // Check if the request origin is in the allowed origins
+    if (this.allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
       'Access-Control-Allow-Methods',
@@ -13,6 +25,7 @@ export class CORSMiddleware implements NestMiddleware {
     );
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    // Handle preflight requests
     if (req.method === 'OPTIONS') {
       return res.status(204).send();
     }
