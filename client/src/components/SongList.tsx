77df4b53eba044394fb551c032cf70b6ce503613
Raftouch <rafanelkin@gmail.com>
2024-11-01 19:26:41 +0100

    improve code for songslist

--- client/src/components/SongList.tsx
+++ client/src/components/SongList.tsx
@@ -1,45 +1,36 @@
-import { Song } from "@/models/song";
-import React from "react";
-import SongCard from "./SongCard";
+import { Song } from '@/models/song'
+import React from 'react'
+import SongCard from './SongCard'
 
 interface SongListProps {
-  songs: Song[] | null;
-  searchParams?: { query?: string; page?: string };
+  songs: Song[] | null
+  searchParams?: { query?: string; page?: string }
 }
 
 export default function SongList({ songs, searchParams }: SongListProps) {
-  const query = searchParams?.query || "";
+  const query = searchParams?.query || ''
 
   if (!songs || songs.length === 0) {
-    return (
-      <div>
-        <p>No songs available.</p>
-      </div>
-    );
+    return <p>No songs available.</p>
   }
 
+  const filteredSongs = query
+    ? songs.filter((song) =>
+        song.title.toLowerCase().startsWith(query.toLowerCase())
+      )
+    : songs
+
   return (
-    <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center">
-      {query ? (
-        <>
-          {songs
-            .filter((song) => {
-              const lowerCaseQuery = query.toLowerCase();
-              return lowerCaseQuery
-              ? song.title.toLowerCase().startsWith(lowerCaseQuery)
-              : true; // Ne filtre pas si la query est vide
-            })
-            .map((song) => (
-              <SongCard song={song} key={song.id} />
-            ))}
-        </>
-      ) : (
-        <>
-          {songs.map((song) => (
+    <>
+      {filteredSongs.length > 0 ? (
+        <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center">
+          {filteredSongs.map((song) => (
             <SongCard song={song} key={song.id} />
           ))}
-        </>
+        </div>
+      ) : (
+        <p>No songs found matching your query.</p>
       )}
-    </div>
-  );
+    </>
+  )
 }
