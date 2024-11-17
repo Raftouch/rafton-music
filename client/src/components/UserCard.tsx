bc3c6435cb860d779e4b82cff6d89ee1aa40bd35
Raftouch <rafanelkin@gmail.com>
2024-11-17 14:23:52 +0100

    updt user card

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import { User } from '@/models/user'
-import { formatName } from '@/utils/format'
+import { formatName, formatDate } from '@/utils/format'
+import Link from 'next/link'
 
 interface UserProps {
   user: User
@@ -8,8 +9,14 @@ interface UserProps {
 
 export default function UserCard({ user, index }: UserProps) {
   return (
-    <li>
-      {index + 1}. {formatName(user.username)}
-    </li>
+    <Link href={`/users/${user.id}`}>
+      <div className="w-full flex justify-between border p-4 rounded-md hover:bg-rafton-green">
+        {/* <p>{index + 1}.</p> */}
+        <p>{formatName(user.username)}</p>
+        <p>{user.email}</p>
+        <p>{formatDate(user.registeredAt)}</p>
+        <p>{user.uploadedSongs?.length}</p>
+      </div>
+    </Link>
   )
 }
