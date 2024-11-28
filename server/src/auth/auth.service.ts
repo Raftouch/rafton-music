b925f2fb3ccd18670877ac6a9ee9cd5589be8acb
raftouch <ntimorshina@gmail.com>
2024-11-28 15:35:16 +0100

    back

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -65,8 +65,7 @@ export class AuthService {
 
     res.cookie('access_token', tokens.access_token, {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 15, // 15 minutes
       // path: '/',
@@ -74,8 +73,7 @@ export class AuthService {
 
     res.cookie('refresh_token', tokens.refresh_token, {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       // path: '/',
@@ -122,8 +120,7 @@ export class AuthService {
 
     res.cookie('access_token', tokens.access_token, {
       httpOnly: true, // Only accessible by the server
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production', // Only send over HTTPS in production
+      secure: process.env.NODE_ENV === 'production', // Only send over HTTPS in production
       sameSite: 'lax',
       maxAge: 1000 * 60 * 15, // 15 minutes
       // path: '/',
@@ -131,8 +128,7 @@ export class AuthService {
 
     res.cookie('refresh_token', tokens.refresh_token, {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       // path: '/',
@@ -158,14 +154,12 @@ export class AuthService {
 
     res.clearCookie('access_token', {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
     });
 
     res.clearCookie('refresh_token', {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
     });
 
     return res.send({ message: 'Logout successful' });
@@ -246,16 +240,14 @@ export class AuthService {
 
     res.cookie('access_token', tokens.access_token, {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 15,
     });
 
     res.cookie('refresh_token', tokens.refresh_token, {
       httpOnly: true,
-      secure: false,
-      // secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
