670d236882fc1cf6f0b97667030b95441b543075
raftouch <ntimorshina@gmail.com>
2024-12-07 21:23:59 +0100

    add validation middleware

--- service/src/routes/logger.ts
+++ service/src/routes/logger.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import LoggerController from "../controllers/logger";
+import { validateLog } from "../middlewares/validateLog";
 
 const router = Router();
 
-router.post("/logs", LoggerController.createLog);
+router.post("/logs", validateLog, LoggerController.createLog);
 router.get("/logs", LoggerController.getAllLogs);
 router.get("/logs/:id", LoggerController.getLog);
 router.patch("/logs/:id", LoggerController.updateLog);
