7a7ccd248316411e9685b8b53d97af1f8de8e05a
raftouch <ntimorshina@gmail.com>
2024-12-07 21:41:07 +0100

    improve logger controller with json output and messages

--- service/src/controllers/logger.ts
+++ service/src/controllers/logger.ts
@@ -5,21 +5,19 @@ export default class LoggerController {
   public static async createLog(req: Request, res: Response): Promise<void> {
     const { eventType, message } = req.body;
 
-    if (!eventType || !message) {
-      res.status(400).send("Event type and message are required");
-      return;
-    }
-
     try {
       const log = new Log({
         eventType,
         message,
       });
       await log.save();
-      res.status(200).send("Log saved");
+      res
+        .status(201)
+        .json({ message: "Log created successfully", logId: log.id });
     } catch (error) {
+      console.error("Error creating log:", error);
       res.status(500).json({
-        error: error instanceof Error ? error.stack : "Error creating log",
+        message: "An error occurred while creating the log.",
       });
     }
   }
@@ -27,10 +25,13 @@ export default class LoggerController {
   public static async getAllLogs(req: Request, res: Response): Promise<void> {
     try {
       const logs = await Log.find();
-      res.status(200).json({ data: logs });
+      res
+        .status(200)
+        .json({ message: "Logs retrieved successfully", data: logs });
     } catch (error) {
+      console.error("Error retrieving logs:", error);
       res.status(500).json({
-        error: error instanceof Error ? error.stack : "Error getting all logs",
+        message: "An error occurred while retrieving logs.",
       });
     }
   }
@@ -42,13 +43,14 @@ export default class LoggerController {
       const log = await Log.findById(id);
 
       if (!log) {
-        res.status(400).send("Log not found");
+        res.status(404).json({ message: "Log not found" });
         return;
       }
-      res.status(200).json(log);
+      res.status(200).json({ message: "Log retrieved successfully", log });
     } catch (error) {
+      console.error("Error retrieving log:", error);
       res.status(500).json({
-        error: error instanceof Error ? error.stack : "Error retrieving log",
+        message: "An error occurred while retrieving the log.",
       });
     }
   }
@@ -65,14 +67,15 @@ export default class LoggerController {
       );
 
       if (!log) {
-        res.status(404).send("Log not found");
+        res.status(404).json({ message: "Log not found" });
         return;
       }
 
-      res.status(200).json(log);
+      res.status(200).json({ message: "Log updated successfully", log });
     } catch (error) {
+      console.error("Error updating log:", error);
       res.status(500).json({
-        error: error instanceof Error ? error.stack : "Error updating log",
+        message: "An error occurred while updating the log.",
       });
     }
   }
@@ -84,13 +87,14 @@ export default class LoggerController {
       const log = await Log.findByIdAndDelete(id);
 
       if (!log) {
-        res.status(404).send("Log not found");
+        res.status(404).json({ message: "Log not found" });
         return;
       }
-      res.status(200).send("Log deleted");
+      res.status(200).json({ message: "Log deleted successfully", log });
     } catch (error) {
+      console.error("Error deleting log:", error);
       res.status(500).json({
-        error: error instanceof Error ? error.stack : "Error deleting log",
+        message: "An error occurred while deleteing the log.",
       });
     }
   }
--- service/src/routes/logger.ts
+++ service/src/routes/logger.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/logs", validateLog, LoggerController.createLog);
 router.get("/logs", LoggerController.getAllLogs);
 router.get("/logs/:id", LoggerController.getLog);
-router.patch("/logs/:id", LoggerController.updateLog);
+router.patch("/logs/:id", validateLog, LoggerController.updateLog);
 router.delete("/logs/:id", LoggerController.deleteLog);
 
 export default router;
