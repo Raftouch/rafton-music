1408c3e3a00f24b0a7e094771101adc77804a256
raftouch <ntimorshina@gmail.com>
2025-01-25 16:45:21 +0100

    fix - reset player on logout

--- client/src/components/LogoutBtn.tsx
+++ client/src/components/LogoutBtn.tsx
@@ -1,35 +1,38 @@
-'use client'
+"use client";
 
-import useUserStore from '@/store/user'
-import { API_URL } from '@/utils/const'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
+import usePlayerStore from "@/store/player";
+import useUserStore from "@/store/user";
+import { API_URL } from "@/utils/const";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function LogoutBtn() {
-  const router = useRouter()
-  const { setUser, setIsAuth } = useUserStore()
+  const router = useRouter();
+  const { setUser, setIsAuth } = useUserStore();
+  const { resetPlayerState } = usePlayerStore();
 
   const handleLogout = async () => {
     try {
       const response = await fetch(`${API_URL}/api/auth/logout`, {
-        method: 'POST',
-        credentials: 'include',
-      })
+        method: "POST",
+        credentials: "include",
+      });
 
       if (!response.ok) {
-        throw new Error(`Logout failed, status: ${response.status}`)
+        throw new Error(`Logout failed, status: ${response.status}`);
       }
 
-      setUser(undefined) // Clear user data
-      setIsAuth(false)
+      setUser(undefined); // Clear user data
+      setIsAuth(false);
+      resetPlayerState();
 
-      toast.success('Logout successful!')
-      router.push('/')
+      toast.success("Logout successful!");
+      router.push("/");
     } catch (error) {
-      console.error(error)
-      toast.error('Logout failed')
+      console.error(error);
+      toast.error("Logout failed");
     }
-  }
+  };
 
-  return <button onClick={handleLogout}>Logout</button>
+  return <button onClick={handleLogout}>Logout</button>;
 }
