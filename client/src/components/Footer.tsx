d66b42d3901558c931995ecd70f0dc99a330e735
raftouch <ntimorshina@gmail.com>
2025-02-04 15:27:56 +0100

    updt footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center fixed bottom-2 text-sm">
+    <footer className="text-center fixed bottom-2 text-xs">
       Created with ❤️ by Nelli - &copy; Copyright {new Date().getFullYear()}{" "}
     </footer>
   );
