2b823c9cbc8eb14fd72022fe73c98fa089149fab
raftouch <ntimorshina@gmail.com>
2025-02-07 16:09:49 +0100

    add image to landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,8 @@
 import Vinyl from "@/components/Vinyl";
+import Image from "next/image";
+import violin from "../../public/images/violin.avif";
+import guitare from "../../public/images/guitare.avif";
+import dog from "../../public/images/dog.avif";
 
 export default function Home() {
   return (
@@ -10,7 +14,13 @@ export default function Home() {
         for creators, by creators
       </h3>
 
-      <p>WHAT A GREAT WEBSITE ❤️</p>
+      <div className="flex">
+        {/* <Image src={violin} alt="violin" width={500} height={250} /> */}
+        <Image src={dog} alt="dog" width={500} height={250} />
+        {/* <Image src={guitare} alt="guitare" width={500} height={250} /> */}
+      </div>
+
+      {/* <p>WHAT A GREAT WEBSITE ❤️</p> */}
     </div>
   );
 }
