50e9f56372fbcd6feee23d211783784b696c5b8f
raftouch <ntimorshina@gmail.com>
2025-02-07 16:22:10 +0100

    updt landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -7,11 +7,11 @@ import dog from "../../public/images/dog.avif";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center mt-20 space-y-10">
-      <h1>Welcome to Rafton</h1>
-      <h2>Share Your Music with the World</h2>
+      <h1>Welcome to Rafton 🙂</h1>
+      <h2>Share Your Music with the World 🌏</h2>
       <h3 className="text-sm">
         Upload your songs, connect with fans, and discover new music. A platform
-        for creators, by creators
+        for creators, by creators ❤️
       </h3>
 
       <div className="flex">
