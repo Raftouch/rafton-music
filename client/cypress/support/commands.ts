4e7d7c38696a0338b7c68bdb372bffc4a44e5f00
raftouch <ntimorshina@gmail.com>
2025-03-16 21:00:56 +0100

    test to be fixed - screenshot attached

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,19 +1,42 @@
-// export {}
+// export {};
 
 // declare global {
 //   namespace Cypress {
 //     interface Chainable {
-//       login(username: string, password: string): Chainable<Element>
+//       login(username: string, password: string): Chainable<Element>;
+//       register(
+//         username: string,
+//         email: string,
+//         password: string
+//       ): Chainable<Element>;
 //     }
 //   }
 // }
 
-// Cypress.Commands.add('login', (username: string, password: string) => {
-//   cy.visit('/')
-//   cy.get('a').contains('Login').click()
-//   cy.url().should('include', '/auth/login')
-//   cy.get('input[name="username"]').type(username)
-//   cy.get('input[name="password"]').type(password)
-//   cy.get('button[type="submit"]').click()
-//   cy.url().should('include', '/songs')
-// })
+// Cypress.Commands.add(
+//   "register",
+//   (username: string, email: string, password: string) => {
+//     cy.visit("/auth/register");
+//     cy.url().should("include", "/auth/register");
+
+//     cy.get('input[name="username"]').type(username);
+//     cy.get('input[name="email"]').type(email);
+//     cy.get('input[name="password"]').type(password);
+//     cy.get('button[type="submit"]').click();
+
+//     cy.wait(30000);
+//     cy.url().should("include", "/login");
+//   }
+// );
+
+// Cypress.Commands.add("login", (username: string, password: string) => {
+//   cy.visit("/");
+//   cy.get("a").contains("Login").click();
+//   cy.url().should("include", "/auth/login");
+//   cy.get('input[name="username"]').type(username);
+//   cy.get('input[name="password"]').type(password);
+//   cy.get('button[type="submit"]').click();
+
+//   cy.wait(10000);
+//   cy.url().should("include", "/songs");
+// });
