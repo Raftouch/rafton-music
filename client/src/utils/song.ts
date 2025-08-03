d029866d8285f83f2d29b87ca60a67c4461404cd
raftouch <rafanelkin@gmail.com>
2025-08-03 16:34:44 +0200

    retrieve public songs on client

--- client/src/utils/song.ts
+++ client/src/utils/song.ts
@@ -1,57 +1,81 @@
-import { Song } from '@/models/song'
-import { notFound } from 'next/navigation'
-import { API_URL } from './const'
+import { Song } from "@/models/song";
+import { notFound } from "next/navigation";
+import { API_URL } from "./const";
 
 export async function getSong(id: string): Promise<Song | null> {
   try {
     const response = await fetch(`${API_URL}/api/songs/${id}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      cache: 'no-store',
-      credentials: 'include',
-    })
+      cache: "no-store",
+      credentials: "include",
+    });
 
     if (response.status === 401) {
-      throw new Error('Unathorized')
+      throw new Error("Unathorized");
     } else if (response.status === 404) {
-      notFound()
+      notFound();
     } else if (!response.ok) {
-      throw new Error('Failed to fetch song data')
+      throw new Error("Failed to fetch song data");
     } else {
-      const song: Song = await response.json()
-      return song
+      const song: Song = await response.json();
+      return song;
     }
   } catch (error) {
-    console.error('Error fetching song:', error)
-    return null
+    console.error("Error fetching song:", error);
+    return null;
   }
 }
 
 export async function getAllSongs(): Promise<Song[]> {
   try {
     const response = await fetch(`${API_URL}/api/songs`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      cache: 'no-store',
-      credentials: 'include',
-    })
+      cache: "no-store",
+      credentials: "include",
+    });
 
     if (response.status === 401) {
-      throw new Error('Unathorized')
+      throw new Error("Unathorized");
     } else if (response.status === 404) {
-      return []
+      return [];
     } else if (!response.ok) {
-      throw new Error(`An error has occurred: ${response.statusText}`)
+      throw new Error(`An error has occurred: ${response.statusText}`);
     } else {
-      const songs: Song[] = await response.json()
-      return songs
+      const songs: Song[] = await response.json();
+      return songs;
     }
   } catch (error) {
-    console.error('Error fetching songs:', error)
-    return []
+    console.error("Error fetching songs:", error);
+    return [];
+  }
+}
+
+export async function getPublicSongs(): Promise<Song[]> {
+  try {
+    const response = await fetch(`${API_URL}/api/songs/public`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+    });
+
+    if (response.status === 404) {
+      return [];
+    } else if (!response.ok) {
+      throw new Error(`An error has occurred: ${response.statusText}`);
+    } else {
+      const songs: Song[] = await response.json();
+      return songs;
+    }
+  } catch (error) {
+    console.error("Error fetching songs:", error);
+    return [];
   }
 }
